30afa75004919fbfc583799e3324717d0f3c6e17
khanglb2000 <lambaokhang2000@gmail.com>
2024-07-19 15:37:29 +0700

    add location producer

--- scripts/deployment_build.sh
+++ scripts/deployment_build.sh
@@ -14,3 +14,14 @@ docker push khanglb/persons:latest
 insert into public.person (id, first_name, last_name, company_name) values (2, 'Hulk', 'Hogan', 'Mania');
 insert into public.person (id, first_name, last_name, company_name) values (3, 'Steve', 'Austin', 'Smakedown');
 insert into public.person (id, first_name, last_name, company_name) values (4, 'Roman', 'Reigns', 'The Bloodline');
+
+-----
+
+# running protoc
+python -m grpc_tools.protoc -I./ --python_out=./ --grpc_python_out=./ location.proto
+
+# location producer
+docker build -t location-producer .
+# docker run -d -p 5555:5555 location-producer --for testing
+docker tag location-producer khanglb/location-producer:latest
+docker push khanglb/location-producer:latest
